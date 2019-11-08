8467586c7b5f2800ad0571f4f31e6595832db0ef
karrettgelley <garrettkelley0@gmail.com>
2019-11-08 15:13:13 -0600

    Complete PageRank calculation

--- webutils/PageRankSpider.java
+++ webutils/PageRankSpider.java
@@ -321,8 +321,118 @@ public class PageRankSpider extends Spider {
         page.write(saveDir, pageNumber);
     }
 
+    /**
+     * Computes the PageRank of all of the indexed documents and stores the values
+     * in page_ranks.txt. The entries in the text file are of the form: P1.html 0.0
+     */
     protected void doPageRank() {
+        // Define PageRank hyperparameters
+        double alpha = 0.15;
+        double rankSource = alpha / count;
+        int iterations = 50;
 
+        // Initialize PageRank of every indexed document
+        HashMap<String, Double> rank = new HashMap<String, Double>();
+        HashMap<String, Double> newRank = new HashMap<String, Double>();
+        initializePageRank(rank, newRank);
+
+        for (int i = 0; i < iterations; i++) {
+            // For every indexed document
+            crawlGraph.resetIterator();
+            Node node = crawlGraph.nextNode();
+            while (node != null) {
+                // Compute the PageRank for the current iteration and store it
+                newRank.put(node.name, getNewPageRank(node, rank, alpha, rankSource));
+
+                node = crawlGraph.nextNode();
+            }
+
+            // Normalize the ranks
+            normalizeRanks(newRank);
+
+            // Update rank with the current iteration PageRank's
+            for (Map.Entry<String, Double> entry : newRank.entrySet()) {
+                rank.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        try {
+            // Write PageRank to page_ranks.txt file
+            PrintWriter out = new PrintWriter(new FileWriter("page_ranks.txt"));
+            for (Map.Entry<String, Double> entry : newRank.entrySet()) {
+                out.println(entry.getKey() + " " + String.valueOf(entry.getValue()));
+            }
+            out.close();
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Initializes the PageRank of the indexed documents.
+     * 
+     * @param rank    A HashMap that maps the name of a document (i.e. P001.html) to
+     *                its PageRank
+     * @param newRank A subsidiary HashMap similar to rank to aid in testing
+     *                PageRank convergence
+     */
+    protected void initializePageRank(HashMap<String, Double> rank, HashMap<String, Double> newRank) {
+        // Iterate every indexed document
+        crawlGraph.resetIterator();
+        Node n = crawlGraph.nextNode();
+        while (n != null) {
+            // Initialize the PageRank to 1/|S| where S is the set of indexed docuemnts
+            rank.put(n.name, 1.0 / count);
+            newRank.put(n.name, 1.0 / count);
+            n = crawlGraph.nextNode();
+        }
+    }
+
+    /**
+     * Normalizes the PageRank of each indexed document.
+     * 
+     * @param newRank a HashMap that maps the name of a document to its PageRank
+     */
+    protected void normalizeRanks(HashMap<String, Double> newRank) {
+        double sumPageRank = 0.0;
+
+        // Compute the ΣR(p) where p is a document and R(p) is the PageRank of document
+        // p
+        for (Map.Entry<String, Double> entry : newRank.entrySet()) {
+            sumPageRank += entry.getValue();
+        }
+
+        // We normalize by 1/ΣR(p)
+        double normalizationFactor = 1.0 / sumPageRank;
+
+        // Normalize the PageRank of every indexed document
+        for (Map.Entry<String, Double> entry : newRank.entrySet()) {
+            newRank.put(entry.getKey(), normalizationFactor * entry.getValue());
+        }
+    }
+
+    /**
+     * Calculates the sum of the PageRanks of the nodes incident to node.
+     * 
+     * @param node the node to be evaluated
+     * @param rank a HashMap that maps a node to its PageRank
+     * @return the sum of the PageRanks of the nodes incident to node
+     */
+    protected double getNewPageRank(Node node, HashMap<String, Double> rank, double alpha, double rankSource) {
+        double sumPageRank = 0.0;
+
+        // Get the list of nodes that are incident to node
+        List<Node> incomingNodes = node.getEdgesIn();
+
+        // Calculate ΣR(p)/N_p where p is a node incidient to node, N is the number of
+        // outlinks from p, and R(p) is the PageRank of p
+        for (Node n : incomingNodes) {
+            sumPageRank += rank.get(n.name) / (n.getEdgesOut().size());
+        }
+
+        double newPageRank = ((1 - alpha) * sumPageRank) + rankSource;
+        return newPageRank;
     }
 
     /**
