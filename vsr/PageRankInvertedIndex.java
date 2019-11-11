5c59b36fbd12918c0990993e8e410a698bbb61e7
karrettgelley <garrettkelley0@gmail.com>
2019-11-11 14:11:47 -0600

    Modularize getPageRank()

--- vsr/PageRankInvertedIndex.java
+++ vsr/PageRankInvertedIndex.java
@@ -8,9 +8,9 @@ import ir.utilities.*;
 import ir.classifiers.*;
 
 /**
- * An inverted index for vector-space information retrieval. Contains methods
- * for creating an inverted index from a set of documents and retrieving ranked
- * matches to queries using standard TF/IDF weighting and cosine similarity.
+ * An inverted index for vector-space information retrieval. Contains methods for creating an
+ * inverted index from a set of documents and retrieving ranked matches to queries using standard
+ * TF/IDF weighting and cosine similarity.
  *
  * @author Ray Mooney
  */
@@ -30,13 +30,13 @@ public class PageRankInvertedIndex extends InvertedIndex {
      * Create an inverted index of the documents in a directory.
      *
      * @param dirFile  The directory of files to index.
-     * @param docType  The type of documents to index (See docType in
-     *                 DocumentIterator)
+     * @param docType  The type of documents to index (See docType in DocumentIterator)
      * @param stem     Whether tokens should be stemmed with Porter stemmer.
      * @param feedback Whether relevance feedback should be used.
      * @param weight   a scaling parameter for PageRank
      */
-    public PageRankInvertedIndex(File dirFile, short docType, boolean stem, boolean feedback, double weight) {
+    public PageRankInvertedIndex(File dirFile, short docType, boolean stem, boolean feedback,
+            double weight) {
         super(dirFile, docType, stem, feedback);
         this.weight = weight;
         this.pageRank = new HashMap<String, Double>();
@@ -44,11 +44,10 @@ public class PageRankInvertedIndex extends InvertedIndex {
     }
 
     /**
-     * Create an inverted index of the documents in a List of Example objects of
-     * documents for text categorization.
+     * Create an inverted index of the documents in a List of Example objects of documents for text
+     * categorization.
      *
-     * @param examples A List containing the Example objects for text categorization
-     *                 to index
+     * @param examples A List containing the Example objects for text categorization to index
      */
     public PageRankInvertedIndex(List<Example> examples) {
         super(examples);
@@ -58,26 +57,12 @@ public class PageRankInvertedIndex extends InvertedIndex {
      * Reads the values from page_ranks.txt into the pageRank HashMap.
      */
     private void getPageRanks() {
-        File[] files;
-
         // Get the page_ranks.txt file from the indexed directory
-        FilenameFilter filenameFilter = new FilenameFilter() {
-
-            @Override
-            public boolean accept(File dir, String name) {
-                if (name.equals("page_ranks.txt")) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        };
-
-        files = dirFile.listFiles(filenameFilter);
+        File file = new File(dirFile.toString(), "page_ranks.txt");
 
         // Read the input from page_ranks.txt to be stored in the map
         try {
-            BufferedReader br = new BufferedReader(new FileReader(files[0]));
+            BufferedReader br = new BufferedReader(new FileReader(file));
             String st;
 
             while ((st = br.readLine()) != null) {
@@ -88,39 +73,18 @@ public class PageRankInvertedIndex extends InvertedIndex {
             System.out.println(e);
             System.exit(0);
         }
+        System.out.println("Read the PageRanks.");
     }
 
     /**
-     * Print out an inverted index by listing each token and the documents it occurs
-     * in. Include info on IDF factors, occurrence counts, and document vector
-     * lengths.
-     */
-    public void print() {
-        // Iterate through each token in the index
-        for (Map.Entry<String, TokenInfo> entry : tokenHash.entrySet()) {
-            String token = entry.getKey();
-            // Print the token and its IDF factor
-            System.out.println(token + " (IDF=" + entry.getValue().idf + ") occurs in:");
-            // For each document referenced, print its name, occurrence count for this
-            // token, and
-            // document vector length (|D|).
-            for (TokenOccurrence occ : entry.getValue().occList) {
-                System.out.println(
-                        "   " + occ.docRef.file.getName() + " " + occ.count + " times; |D|=" + occ.docRef.length);
-            }
-        }
-    }
-
-    /**
-     * Calculate the final score for a retrieval and return a Retrieval object
-     * representing the retrieval with its final score.
+     * Calculate the final score for a retrieval and return a Retrieval object representing the
+     * retrieval with its final score.
      *
-     * @param queryLength The length of the query vector, incorporated into the
-     *                    final score
+     * @param queryLength The length of the query vector, incorporated into the final score
      * @param docRef      The document reference for the document concerned
      * @param score       The partially computed score
-     * @return The retrieval object for the document described by docRef and score
-     *         under the query with length queryLength
+     * @return The retrieval object for the document described by docRef and score under the query
+     *         with length queryLength
      */
     protected Retrieval getRetrieval(double queryLength, DocumentReference docRef, double score) {
         // Normalize score for the lengths of the two document vectors
@@ -134,12 +98,11 @@ public class PageRankInvertedIndex extends InvertedIndex {
     }
 
     /**
-     * Index a directory of files and then interactively accept retrieval queries.
-     * Command format: "InvertedIndex [OPTION]* [DIR]" where DIR is the name of the
-     * directory whose files should be indexed, and OPTIONs can be "-html" to
-     * specify HTML files whose HTML tags should be removed. "-stem" to specify
-     * tokens should be stemmed with Porter stemmer. "-feedback" to allow relevance
-     * feedback from the user.
+     * Index a directory of files and then interactively accept retrieval queries. Command format:
+     * "InvertedIndex [OPTION]* [DIR]" where DIR is the name of the directory whose files should be
+     * indexed, and OPTIONs can be "-html" to specify HTML files whose HTML tags should be removed.
+     * "-stem" to specify tokens should be stemmed with Porter stemmer. "-feedback" to allow
+     * relevance feedback from the user.
      */
     public static void main(String[] args) {
         // Parse the arguments into a directory name and optional flag
@@ -167,7 +130,8 @@ public class PageRankInvertedIndex extends InvertedIndex {
         }
 
         // Create an inverted index for the files in the given directory.
-        PageRankInvertedIndex index = new PageRankInvertedIndex(new File(dirName), docType, stem, feedback, weight);
+        PageRankInvertedIndex index =
+                new PageRankInvertedIndex(new File(dirName), docType, stem, feedback, weight);
         // index.print();
         // Interactively process queries to this index.
         index.processQueries();
